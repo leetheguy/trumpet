07e12610a7360811d8be83388e342723743c07e7
Lee Nathan <leetheguy@gmail.com>
2013-03-09 13:40:42 -0800

    added first method

--- lib/trumpet/user_additions.rb
+++ lib/trumpet/user_additions.rb
@@ -5,20 +5,28 @@ module Trumpet
       extend ClassMethods
     end
     module InstanceMethods
-      def hello
-        puts 'hello squril'
+      def increase_points(value, type)
+        @trumpet_rule = TrumpetRule.new
+        points = self.class.find_or_create_points(type)
+        points.total += value
+        points.save
+        rules_levels = rule_sheet.trumpet_levels[type]
+        if rules_levels
+          level = self.class.find_or_create_level(type)
+          ttl = rules_levels.find_index{ |l| points.total < l }
+          level.total = ttl || rules_levels.count
+          level.save
+        end
+        points.total
       end
     end
     module ClassMethods
-      def find_or_create_points(type, model)
+      def find_or_create_points(type)
         if (tp = TrumpetRule.where(acts_as_level: false, acts_as_achievement: false).find_by_name(type))
           tp
         else
           TrumpetRule.create(name: type, total: 0)
         end
-      end
-      def test
-        puts 'test'
       end
     end
   end
