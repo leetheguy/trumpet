76c7845b8b2ab3807d06548d81fb5085afb3f4f5
Lee Nathan <leetheguy@gmail.com>
2013-03-09 13:32:06 -0800

    added first method

--- lib/trumpet/user_additions.rb
+++ lib/trumpet/user_additions.rb
@@ -10,6 +10,13 @@ module Trumpet
       end
     end
     module ClassMethods
+      def find_or_create_points(type, model)
+        if (tp = TrumpetRule.where(acts_as_level: false, acts_as_achievement: false).find_by_name(type))
+          tp
+        else
+          TrumpetRule.create(name: type, total: 0)
+        end
+      end
       def test
         puts 'test'
       end
